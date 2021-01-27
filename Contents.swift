e1015ef19ade8ac8ad845e976b9852cbf4cd3798
BOB <boburkosimov0@gmail.com>
2021-01-27 02:35:58 +0200

    first commit

---
+++ Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var str = "Hello, playground"
+  
+// MARK: - LESSON 1
+ 
+// EXERCISE 2.2
+ 
+ let myAge = 25
+
+let YearsToAdd = 10
+var myAgeInTEnYEars = myAge + YearsToAdd
+
+
+// exercise 2.3
+let daysInYear = 365.25
+
+ var daysPassed = Float(myAgeInTEnYEars) * Float(daysInYear)
+print(daysPassed)
+
+// exercise 3
+  
+let acSide = 8.0
+let cbSide = 6.0
+
+let sidesSquareSum = pow(acSide, 2) + pow(cbSide, 2)
+let abHypotenuseLength = sidesSquareSum.squareRoot()
+
+let squareArea = (acSide + cbSide + abHypotenuseLength)/2
+
+let perimeter = acSide + cbSide + abHypotenuseLength
+ 
+    
+      
+
+
+
+
+ 
+ 
+ 
